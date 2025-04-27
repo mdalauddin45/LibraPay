b5e0629a37595b888838308b29a146cfb10fbce9
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2025-04-27 23:26:53 +0600

    book added

--- LibraPay/urls.py
+++ LibraPay/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+    path('home/<slug:category_slug>/', HomeView.as_view(), name='category_wise_post'),
     path('accounts/', include('accounts.urls')),
     path('books/', include('book.urls')),
 ]
