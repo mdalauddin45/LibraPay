02e0a6bfe47d07ebfad7eaaaf1c6833bf7c9b2d5
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2025-04-27 22:05:22 +0600

    first commit

---
+++ LibraPay/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from core.views import HomeView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
